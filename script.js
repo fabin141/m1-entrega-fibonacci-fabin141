dc6e35394c3eafcfc13b07f3f2a6726ca842ebac
Fabio <fabiowillian.shr@gmail.com>
2022-08-23 11:26:42 -0300

    Finalizando Entrega-Extra/Fibonnacci

---
+++ script.js
@@ -0,0 +1,33 @@
+function soma(num1,num2){
+    return num1+num2
+}
+function fibonacci(sequencia){
+    if(sequencia===1){
+      return 1
+    }
+    let anterior = 0;
+    let atual = 1;
+    let resultado = 0;
+    for(let i = 1; i < sequencia; i++){
+      resultado = soma(anterior,atual);
+      anterior = atual;
+      atual = resultado;
+    }
+  return resultado;
+  }
+
+  console.log(fibonacci(8));
+
+
+//Experiência dois;
+
+const fib = (n) =>{
+    const table = Array(n + 1).fill(0);
+    table[1] = 1;
+    for(let i = 0; i <= n; i++){
+        table [i + 1] += table[i];
+        table [i + 2] += table[i];
+    }
+    return table[n];
+}
+console.log(fib(8));
